8f7add61c7af77d59065fb22a88227c6188b560e
mengmiao <mengmiao@163.com>
2016-08-30 13:19:49 +0800

    this is first commit

---
+++ src/com/servlet/Verify.java
@@ -0,0 +1,59 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+/**
+ * Servlet implementation class Verify
+ */
+@WebServlet("/Verify")
+public class Verify extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Verify() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		PrintWriter pw = response.getWriter();
+		int time = session.getMaxInactiveInterval();
+		Date T=new Date(session.getLastAccessedTime());
+		System.out.println(session.getId() + "\n" + session.isNew() + "\n" + time+"\n"+T);
+		if (session.isNew()) {
+			pw.println("这是新会话");
+		} else {
+			pw.println("这是旧会话");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
